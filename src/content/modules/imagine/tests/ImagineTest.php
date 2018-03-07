20126ad9a143007903ac3eff3a821a6a57d502ea
Ulrich Schmidt <admin@deruli.de>
2018-03-07 18:42:05 +0100

    refakturierung

--- src/content/modules/imagine/tests/ImagineTest.php
+++ src/content/modules/imagine/tests/ImagineTest.php
@@ -1,12 +1,9 @@
 <?php
 use Imagine\Imagick\Image;
-
-class ImagineTest extends PHPUnit_Framework_TestCase
-{
-
+class ImagineTest extends PHPUnit_Framework_TestCase {
+    
     // Test if Imagine is correctly installed
-    public function testImagineInstalled()
-    {
-        $this->assertTrue(class_exists('\Imagine\Imagick\Image'));
+    public function testImagineInstalled() {
+        $this->assertTrue ( class_exists ( '\Imagine\Imagick\Image' ) );
     }
 }
