5c9d0a3895a25875d62870cddf74522e8f8da3dc
Ulrich Schmidt <admin@deruli.de>
2018-03-07 18:40:20 +0100

    test funktioniert

---
+++ src/content/modules/imagine/tests/ImagineTest.php
@@ -0,0 +1,12 @@
+<?php
+use Imagine\Imagick\Image;
+
+class ImagineTest extends PHPUnit_Framework_TestCase
+{
+
+    // Test if Imagine is correctly installed
+    public function testImagineInstalled()
+    {
+        $this->assertTrue(class_exists("Imagine"));
+    }
+}
